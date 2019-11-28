7d4c53172f03f36a760b823d82b62f1547f72148
josemnr <maninari97@gmail.com>
2019-11-27 20:46:06 -0600

    Updating Activity_Services

--- app/src/main/java/com/example/proyecto_moviles/activities/Activity_Services.kt
+++ app/src/main/java/com/example/proyecto_moviles/activities/Activity_Services.kt
@@ -8,19 +8,70 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto_moviles.R
 import com.example.proyecto_moviles.adapters.AdapterService
+import com.parse.ParseObject
+import com.parse.ParseQuery
 import org.jetbrains.anko.*
 import kotlin.random.Random
 
 class Activity_Services : AppCompatActivity(){
+
+    private lateinit var addServiceButton: Button
+    private lateinit var doneButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_services)
 
         val recyclerView = findViewById<RecyclerView>(R.id.services_RecyclerView)
-        val services = arrayListOf<HashMap<String, String>>()
+        val services = arrayListOf<String>()
 
-        recyclerView.adapter = AdapterService(services)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter = AdapterService(getParseServices())
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+
+        addServiceButton = findViewById(R.id.AddService_button_activityServices)
+        doneButton = findViewById(R.id.Done_button_activityServices)
+
+        addServiceButton.setOnClickListener(){
+            startActivity<Activity_newService>()
+        }
+
+        doneButton.setOnClickListener(){
+            startActivity<ActivityMain>()
+        }
+
+
+        /********************************************************************
+         * IMPLEMENT PARSE FETCH SERVICES HERE
+         * *******************************************************************
+         */
+
+        val service: String
+        val ParseObject = ParseObject("Service")
+        val query = ParseQuery.getQuery<ParseObject>("Service")
+        query.getFirstInBackground { obj, e ->
+            if (e == null) {
+                services.add(obj.get("NameOfService") as String)
+                recyclerView.adapter = AdapterService(services)
+                recyclerView.layoutManager = LinearLayoutManager(this)
+            }
+        }
     }
+
+    /*
+    fun getParseServices() : ArrayList<ParseObject> {
+        val service: String
+        val services = arrayListOf<ParseObject>()
+        val ParseObject = ParseObject("Service")
+        val query = ParseQuery.getQuery<ParseObject>("Service")
+        query.getFirstInBackground { obj, e ->
+            if (e == null) {
+                services.add(obj)
+                //recyclerView.adapter = AdapterService(services)
+                //recyclerView.layoutManager = LinearLayoutManager(this)
+            }
+        }
+        return services
+    }
+
+     */
 }
--- app/src/main/java/com/example/proyecto_moviles/adapters/AdapterService.kt
+++ app/src/main/java/com/example/proyecto_moviles/adapters/AdapterService.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyecto_moviles.R
 
-class AdapterService(private val services: ArrayList<HashMap<String, String>>): RecyclerView.Adapter<ServiceViewHolder>(){
+/*class AdapterService(private val services: ArrayList<HashMap<String, String>>): RecyclerView.Adapter<ServiceViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServiceViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.individual_service, parent, false)
 
@@ -27,3 +27,31 @@ class ServiceViewHolder(view: View): RecyclerView.ViewHolder(view) {
         serviceName.text = service.get("name")
     }
 }
+
+ */
+
+/////////////////////////////////////
+
+class AdapterService (private val service_names: ArrayList<String>):RecyclerView.Adapter<NameViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NameViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.individual_service, parent, false)
+        return NameViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = service_names.size
+
+    override fun onBindViewHolder(holder: NameViewHolder, position: Int) {
+        holder.bind(service_names[position])
+    }
+}
+
+class NameViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+    private val nameTitle: TextView = view.findViewById(R.id.item_title_name)
+
+    fun bind(service: String) {
+        nameTitle.text = service
+    }
+}
+
